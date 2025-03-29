cf8c0c95f2d6daec3d4c4c1e19a2c6f2e004dde6
Mehedi hasan <nodeNINJAr@users.noreply.github.com>
2025-03-29 14:19:37 +0600

    auth-and-chess-board-making

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,37 @@
-import Image from "next/image";
+// app/page.js
+'use client';
+import ChessBoard from "@/components/ChessBoard";
+
+ // Client component since it uses hooks
 
 export default function Home() {
+
+
+
+
+
+  // 
   return (
-    <>
-      <h1>eeee</h1>
-    </>
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+      <h1>Chess Game</h1>
+      <input
+        value=""
+        // onChange={(e) => setPlayerName(e.target.value)}
+        placeholder="Your name"
+      />
+      <button>
+        Create Game
+      </button>
+      <input
+        value=""
+        // onChange={(e) => setGameId(e.target.value)}
+        placeholder="Enter Game ID"
+      />
+      <button className='cursor-pointer'>
+        Join Game
+      </button>
+
+      <ChessBoard/>
+    </div>
   );
 }
