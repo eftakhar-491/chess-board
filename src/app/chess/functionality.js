0e9ab5409489ae8516d9d52af75e0a047361c50d
MD EFTAKHARUL ISLAM KHAN <eftt042@gmail.com>
2025-03-29 11:37:39 +0600

    EFT_folder stracture create

---
+++ src/app/chess/functionality.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function functionality() {
+  return <div>functionality</div>;
+}
---
+++ src/app/chess/page.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function page() {
+  // array dimu print hobe
+  return <div>chess</div>;
+}
